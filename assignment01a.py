c5b1e59aee5637746be41a23cdc9937b75efbd4b
Petro <chynnyk@vivladi.net>
2019-09-24 19:50:25 +0300

    Version 1.1 task a

--- assignment01a.py
+++ assignment01a.py
@@ -7,51 +7,26 @@ For those who are already familiar with Python – write the best code you can t
 
 """
 
-text = '''
-That kept the cock that crow'd in the morn, 
-That waked the priest all shaven and shorn,
-That married the man all tatter'd and torn, 
-That kissed the maiden all forlorn, 
-That milk'd the cow with the crumpled horn,
-That tossed the dog, 
-That worried the cat, 
-That killed the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-'''
+verb = ['This is','That kept','That waked','That married','That kissed','That milked','That tossed','That worried','That killed','That ate','That lay']
+noun = ['the farmer', 'the cock', 'the priest', 'the man', 'the maiden', 'the cow', 'the dog', 'the cat', 'the rat', 'the malt', 'the house']
+attribute = ['sowing his corn','that crowed in the morn','all shaven and shorn','all tattered and torn','all forlorn','with the crumpled horn', 'that Jack built']
 
-poem = f'''
-This is the house that Jack built.
+#Creating sentences to start a verse 
+sentence_start = [f'{verb[0]} {noun[i]} {attribute[i] if i < len(attribute)-1 else ""}' for i in range(len(noun))]
+sentence_start[-1] += attribute[-1]
+#Creating verse sentences
+sentence = [f'{verb[i+1]} {noun[i+1]} {attribute[i+1] if i+1 < len(attribute)-1 else ""}' for i in range(len(noun)-1)]
+sentence[-1] += attribute[-1]
 
-This is the malt 
-{text[302:]}
-
-This is the rat, 
-{text[283:]}
-
-This is the cat, 
-{text[261:]}
-
-This is the dog, 
-{text[238:]}
-
-This is the cow with the crumpled horn, 
-{text[216:]}
-
-This is the maiden all forlorn,
-{text[172:]}
-
-This is the man all tatter'd and torn, 
-{text[135:]}
-
-This is the priest all shaven and shorn, 
-{text[90:]}
-
-This is the cock that crow'd in the morn, 
-{text[46:]}
-
-This is the farmer sowing his corn, 
-{text[1:]}
-'''
+i = -1
+poem = f'{sentence_start[-1]}\n'
+#Creating poem
+while (i > -len(sentence_start)):
+    poem += f'\n{sentence_start[i-1]}\n'
+    j = i
+    while (j <= -1):
+        poem += f'{sentence[j]}\n'
+        j += 1
+    i -= 1
 
 print(poem)
